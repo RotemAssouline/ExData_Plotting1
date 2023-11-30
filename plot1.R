cb4343ef7902921c2c2572d5b6d5b452ac2135c8
unknown <rotemassouline@gmail.com>
2023-11-30 10:15:57 +0200

    Fixed scripts to unzip data file

--- plot1.R
+++ plot1.R
@@ -2,7 +2,9 @@
 
 # read the data
 
-  hpc <- read.table('household_power_consumption.txt',sep = ';',header = TRUE, dec = '.', na.strings = '?', colClasses = c(rep('character',2),rep('numeric',7)))
+  hpc <- read.table(unz('exdata_data_household_power_consumption.zip','household_power_consumption.txt'),
+                    sep = ';',header = TRUE, dec = '.', na.strings = '?',
+                    colClasses = c(rep('character',2),rep('numeric',7)))
   hpc <- hpc[hpc$Date %in% c('1/2/2007','2/2/2007'),]
   DateTime <- strptime(paste(hpc$Date,hpc$Time),format = '%d/%m/%Y %H:%M:%S')
   hpc <- cbind(DateTime,hpc[,c(-1,-2)])
--- plot4.R
+++ plot4.R
@@ -2,7 +2,9 @@
 
 # read the data 
 
-  hpc <- read.table('household_power_consumption.txt',sep = ';',header = TRUE, dec = '.', na.strings = '?', colClasses = c(rep('character',2),rep('numeric',7)))
+  hpc <- read.table(unz('exdata_data_household_power_consumption.zip','household_power_consumption.txt'),
+                  sep = ';',header = TRUE, dec = '.', na.strings = '?',
+                  colClasses = c(rep('character',2),rep('numeric',7)))
   hpc <- hpc[hpc$Date %in% c('1/2/2007','2/2/2007'),]
   DateTime <- strptime(paste(hpc$Date,hpc$Time),format = '%d/%m/%Y %H:%M:%S')
   hpc <- cbind(DateTime,hpc[,c(-1,-2)])
@@ -14,7 +16,7 @@
   
   par(mfcol = c(2,2))
   
-  plot(hpc$DateTime,hpc$Global_active_power,type = 'l',xlab='',xaxt ='n',ylab = 'Global Active Power (killowatts)')
+  plot(hpc$DateTime,hpc$Global_active_power,type = 'l',xlab='',xaxt ='n',ylab = 'Global Active Power')
   axis.POSIXct(1, at=as.Date(hpc$DateTime),labels = format(hpc$DateTime,'%a'))
   plot(hpc$DateTime,hpc$Sub_metering_1,type = 'l',xlab = '',ylab = 'Energy sub metering', xaxt = 'n' )
   lines(hpc$DateTime,hpc$Sub_metering_2,col = 'red')
